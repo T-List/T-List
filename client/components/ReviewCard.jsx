0ae7ab23c73acac8d748d9246a2a05a177d1df3e
Peter Steinberg <bklynpeter@gmail.com>
2022-04-27 13:46:47 -0400

    made admin jibe w Em work; added delete to review but no functionality

--- client/components/ReviewCard.jsx
+++ client/components/ReviewCard.jsx
@@ -1,25 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 const ReviewCard = (props) => {
-    const { rating, service_type, cost, review } = props;
-    // display rating, service type, cost, review
-    const starObj = {
-        1: '✩',
-        2: '✩✩',
-        3: '✩✩✩',
-        4: '✩✩✩✩',
-        5: '✩✩✩✩✩'
-    }
+  const { rating, service_type, cost, review } = props
+  // display rating, service type, cost, review
+  const starObj = {
+    1: '✩',
+    2: '✩✩',
+    3: '✩✩✩',
+    4: '✩✩✩✩',
+    5: '✩✩✩✩✩',
+  }
 
-    return(
-        <div className="reviewCard">
-            <p className="reviewText"><strong>Rating: </strong>{starObj[String(rating)]}</p>
-            <p className="reviewText"><strong>Service Type: </strong>{service_type}</p>
-            <p className="reviewText"><strong>Cost: </strong>{cost}</p>
-            <p className="reviewText"><strong>Review: </strong>{review}</p>
-            <hr />
-        </div>
-    )
+  return (
+    <div className="reviewCard">
+      <p className="reviewText">
+        <strong>Rating: </strong>
+        {starObj[String(rating)]}
+      </p>
+      <p className="reviewText">
+        <strong>Service Type: </strong>
+        {service_type}
+      </p>
+      <p className="reviewText">
+        <strong>Cost: </strong>
+        {cost}
+      </p>
+      <p className="reviewText">
+        <strong>Review: </strong>
+        {review}
+      </p>
+      <button className="reviewCardDelete">x</button>
+      <hr />
+    </div>
+  )
 }
 
-export default ReviewCard;
+export default ReviewCard
--- client/containers/MapContainer.jsx
+++ client/containers/MapContainer.jsx
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
 //apiKey: 'AIzaSyAJdQ - ID6_clf4WGWk5F8bt3CnNMlHCXRs'\
 
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Wrapper, Status } from '@googlemaps/react-wrapper';
-import MapComponent from '../components/MapComponent.jsx';
-import Marker from '../components/Marker.jsx';
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import { Wrapper, Status } from '@googlemaps/react-wrapper'
+import MapComponent from '../components/MapComponent.jsx'
+import Marker from '../components/Marker.jsx'
 import Sidebar from '../components/Sidebar.jsx'
 
 // when we have other components built out, remember to import them here
@@ -15,68 +15,73 @@ const MapContainer = (props) => {
     return <h1>{status}</h1>
   }
 
-	const [markers, setMarkers] = React.useState([])
-	const [coords, setCoords] = React.useState();
-	
-	// gets markers from database
-	React.useEffect(() => {
-		if (!markers.length) {
-			fetch('/api')
-			.then(resp => resp.json())
-			.then(data => {
-				setMarkers(data)
-			})
-		}
-	}, [])
+  const [markers, setMarkers] = React.useState([])
+  const [coords, setCoords] = React.useState()
 
-	const changeCoords = (lat, lng) => {
-		setCoords([lat, lng])
-	}
-	
+  // gets markers from database
+  React.useEffect(() => {
+    if (!markers.length) {
+      fetch('/api')
+        .then((resp) => resp.json())
+        .then((data) => {
+          setMarkers(data)
+        })
+    }
+  }, [])
 
-	// TODO: function to pass down to markers which will pull up the reviews associated with that marker upon click
-	const [reviews, setReviews] = React.useState([]);
+  const changeCoords = (lat, lng) => {
+    setCoords([lat, lng])
+  }
 
-	const loadReviews = (reviews) => {
-		setReviews(reviews);
-		console.log("reviews in loadreviews", reviews)
-	}
+  // TODO: function to pass down to markers which will pull up the reviews associated with that marker upon click
+  const [reviews, setReviews] = React.useState([])
 
+  const loadReviews = (reviews) => {
+    setReviews(reviews)
+    console.log('reviews in loadreviews', reviews)
+  }
 
-	// generates marker components from marker array in state
-	const markersArray = markers.map(marker => {
-	return (
-	<Marker 
-		changeCoords= {changeCoords}
-		loadReviews= {loadReviews}
-		key = {marker._id}
-		id = {marker._id}
-		clinicName = {marker.clinic}
-		position = {{
-			lat: Number(marker.latitude), 
-			lng: Number(marker.longitude),
-		}}
-		/>
-		)
-	})
-	return (
-		<>
-		<Wrapper apiKey={'AIzaSyCpR5rGEJvMPMhDR4kUNdlXm27tbJX_7mY'} render={render}>
-			<div className='mapComponent'>
-				<MapComponent
-					changeCoords = {changeCoords}
-					coords = {coords}
-				>
-					{markersArray}
-				</MapComponent>
-			</div>
-			<Sidebar
-				reviews={reviews}
-			/>
-		</Wrapper>
-		</>
-	);
-};
-
+  // generates marker components from marker array in state
+  const markersArray = markers.map((marker) => {
+    return (
+      <Marker
+        changeCoords={changeCoords}
+        loadReviews={loadReviews}
+        key={marker._id}
+        id={marker._id}
+        clinicName={marker.clinic}
+        position={{
+          lat: Number(marker.latitude),
+          lng: Number(marker.longitude),
+        }}
+      />
+    )
+  })
+  return (
+    <>
+      <Wrapper
+        apiKey={'AIzaSyCpR5rGEJvMPMhDR4kUNdlXm27tbJX_7mY'}
+        render={render}
+      >
+        <div className="mapComponent">
+          <MapComponent
+            changeCoords={changeCoords}
+            coords={coords}
+            className="mapComponent"
+          >
+            {markersArray}
+          </MapComponent>
+          <button
+            className="seekingAdmin"
+            onClick={() => props.turnOnSeekingAdmin()}
+          >
+            admin
+          </button>
+        </div>
+        <Sidebar reviews={reviews} />
+      </Wrapper>
+    </>
+  )
+}
 
 export default MapContainer
