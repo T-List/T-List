50d0cc720f463b3758f0736fd886ea55312e9eca
Zachary Radford <101583420+zachradf@users.noreply.github.com>
2022-04-27 11:18:58 -0400

    changed api key and pg uri

--- client/components/MapComponent.jsx
+++ client/components/MapComponent.jsx
@@ -1,77 +1,76 @@
-import React, {useState} from 'react';
-// import GoogleMapReact from 'google-map-react';
-import { InfoWindow } from 'google-maps-react';
+import React, { useState } from "react";
+import GoogleMapReact from "google-map-react";
+import { InfoWindow } from "google-maps-react";
 // import { Link, Navigate } from 'react-router-dom';
-import CreateReview from './CreateReview.jsx';
-
+import CreateReview from "./CreateReview.jsx";
 
 const MapComponent = ({ changeCoords, children, coords }) => {
-	const ref = React.useRef(null);
-	const [map, setMap] = React.useState();
+  const ref = React.useRef(null);
+  const [map, setMap] = React.useState();
 
-	const infoWindow = new google.maps.InfoWindow({
-		content: 'To leave a review at this location, click the "Post a review" button below!'
-	})
+  const infoWindow = new google.maps.InfoWindow({
+    content:
+      'To leave a review at this location, click the "Post a review" button below!',
+  });
 
-	const [review, setReview] = React.useState(false)
+  const [review, setReview] = React.useState(false);
 
-	React.useEffect(() => {
-		if (ref.current && !map) {
-			setMap(
-				new window.google.maps.Map(ref.current, {
-					center: { lat: 40.734417940592024, lng: -74.0021626},
-					zoom: 12,
-				})
-			);
-		}
+  React.useEffect(() => {
+    if (ref.current && !map) {
+      setMap(
+        new window.google.maps.Map(ref.current, {
+          center: { lat: 40.734417940592024, lng: -74.0021626 },
+          zoom: 12,
+        })
+      );
+    }
 
-		if (map) {
-			map.addListener('click', (mapsMouseEvent) => {
-				const mapClickLat = mapsMouseEvent.latLng.lat();
-				const mapClickLng = mapsMouseEvent.latLng.lng();
-				// console.log(mapClickLat, mapClickLng);
-				const marker = new google.maps.Marker({
-					position: {lat: mapClickLat, lng: mapClickLng},
-					map: map
-				})
-				infoWindow.open({
-					anchor: marker,
-					map: map,
-					shouldFocus: true,
-				});
-				changeCoords([mapClickLat, mapClickLng], null)
-			});
-		}
-	}, [ref, map]);
+    if (map) {
+      map.addListener("click", (mapsMouseEvent) => {
+        const mapClickLat = mapsMouseEvent.latLng.lat();
+        const mapClickLng = mapsMouseEvent.latLng.lng();
+        // console.log(mapClickLat, mapClickLng);
+        const marker = new google.maps.Marker({
+          position: { lat: mapClickLat, lng: mapClickLng },
+          map: map,
+        });
+        infoWindow.open({
+          anchor: marker,
+          map: map,
+          shouldFocus: true,
+        });
+        changeCoords([mapClickLat, mapClickLng], null);
+      });
+    }
+  }, [ref, map]);
 
-	const style = {
-		width: '1000px',
-		height: '800px',
-	};
-	const reviewForm = []
-	if (review) {
-		reviewForm.push(<CreateReview coords={coords} />)
-	}
+  const style = {
+    width: "1000px",
+    height: "800px",
+  };
+  const reviewForm = [];
+  if (review) {
+    reviewForm.push(<CreateReview coords={coords} />);
+  }
 
-	return (
-		<>
-			<div ref={ref} style={style} />
-			{React.Children.map(children, (child) => {
-				if (React.isValidElement(child)) {
-				
-					return React.cloneElement(child, { map });
-				}
-			})}
-			<button
-			className='review-btn'
-			style={{ width: '200px', height: '30px' }}
-			onClick = {()=> setReview(true)}
-		>
-			Post a Review
-		</button>
-			{reviewForm}
-		</>
-	);
+  return (
+    <>
+      <div ref={ref} style={style} />
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, { map });
+        }
+      })}
+      <button
+        className="review-btn"
+        style={{ width: "200px", height: "30px" }}
+        onClick={() => setReview(true)}
+      >
+        Post a Review
+      </button>
+      {reviewForm}
+    </>
+  );
 };
 
 export default MapComponent;
--- client/containers/MapContainer.jsx
+++ client/containers/MapContainer.jsx
@@ -1,59 +1,59 @@
 //apiKey: 'AIzaSyAJdQ - ID6_clf4WGWk5F8bt3CnNMlHCXRs'\
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Wrapper, Status } from '@googlemaps/react-wrapper';
-import MapComponent from '../components/MapComponent.jsx';
-import Marker from '../components/Marker.jsx';
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import MapComponent from "../components/MapComponent.jsx";
+import Marker from "../components/Marker.jsx";
 // when we have other components built out, remember to import them here
 
 const MapContainer = () => {
-	const render = (status) => {
-		return <h1>{status}</h1>;
-	};
+  const render = (status) => {
+    return <h1>{status}</h1>;
+  };
 
-	const [markers, setMarkers] = React.useState([])
-	const [coords, setCoords] = React.useState();
-	
-	React.useEffect(() => {
-		if (!markers.length) {
-			fetch('/api')
-			  .then(resp => resp.json())
-			  .then(data => {
-				  setMarkers(data)
-			  })
-		}
-	}, [])
+  const [markers, setMarkers] = React.useState([]);
+  const [coords, setCoords] = React.useState();
 
-	const changeCoords = (lat, lng) => {
-		setCoords([lat, lng])
-	}
+  React.useEffect(() => {
+    if (!markers.length) {
+      fetch("/api")
+        .then((resp) => resp.json())
+        .then((data) => {
+          setMarkers(data);
+        });
+    }
+  }, []);
 
-	const markersArray = markers.map(marker => {
-	return (
-	<Marker 
-		changeCoords= {changeCoords}
-		key = {marker._id}
-		id = {marker._id}
-		clinicName = {marker.clinic}
-		position = {{
-			lat: Number(marker.latitude), 
-			lng: Number(marker.longitude),
-		}}
-		/>
-		)
-	})
-	return (
-		<>
-		<Wrapper apiKey={'AIzaSyAJdQ-ID6_clf4WGWk5F8bt3CnNMlHCXRs'} render={render}>
-			<MapComponent
-				changeCoords = {changeCoords}
-				coords = {coords}
-			>
-				{markersArray}
-			</MapComponent>
-		</Wrapper>
-		</>
-	);
+  const changeCoords = (lat, lng) => {
+    setCoords([lat, lng]);
+  };
+
+  const markersArray = markers.map((marker) => {
+    return (
+      <Marker
+        changeCoords={changeCoords}
+        key={marker._id}
+        id={marker._id}
+        clinicName={marker.clinic}
+        position={{
+          lat: Number(marker.latitude),
+          lng: Number(marker.longitude),
+        }}
+      />
+    );
+  });
+  return (
+    <>
+      <Wrapper
+        apiKey={"AIzaSyCpR5rGEJvMPMhDR4kUNdlXm27tbJX_7mY"}
+        render={render}
+      >
+        <MapComponent changeCoords={changeCoords} coords={coords}>
+          {markersArray}
+        </MapComponent>
+      </Wrapper>
+    </>
+  );
 };
 
 export default MapContainer;
