2e75af8f21d3b26c11b9b96c6a02e3bd565fbdd2
Ricky Johnson <rickyjohnson@Rickys-MBP.lan>
2022-04-27 11:00:54 -0400

    testing setup

--- client/__tests__/react.js
+++ client/__tests__/react.js
@@ -2,11 +2,9 @@
  * @jest-environment jsdom
  */
 
-//failed tests swallow console log but can see in successes
-
 import React from 'React';
 import userEvent from '@testing-library/user-event';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import regeneratorRuntime from 'regenerator-runtime';
 
 // import App from '../../components/App';
@@ -16,13 +14,29 @@ import Marker from '../components/Marker';
 
 describe('Unit testing react components', () => {
   describe('CreateReview', () => {
-    test('Input boxes - 4 inputs and 1 text area', async () => {
-      render(<CreateReview />);
+    let review;
+    beforeEach(() => {
+      //Have to render beforeEach test
+      review = render(<CreateReview />);
+    });
+
+    test('Form should have five input boxes (including text area)', async () => {
+      // render(<CreateReview />);
       const inputs = await screen.findAllByRole('textbox');
       expect(inputs.length).toBe(5);
-      // console.debug('ricky');
-      // console.log(inputs.length);
+      // const reviewLabelTest = await screen.getByText('Review:');
       // screen.debug();
     });
+
+    test('Form should display labels of Clinic, Type of Service, Cost, Rating, Review:', () => {
+      expect(review.getByText('Clinic:')).toBeTruthy();
+      expect(review.getByText('Type of Service:')).toBeTruthy();
+      expect(review.getByText('Cost:')).toBeTruthy();
+      expect(review.getByText('Rating:')).toBeTruthy();
+      expect(review.getByText('Review:')).toBeTruthy();
+    });
+    test('Mock Form submit button fire event handles error', () => {});
+    //Mock Form submit button fire event and received error request
+    //Explore mocking and use cases
   });
 });
