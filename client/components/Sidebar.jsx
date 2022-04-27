e0d0659c38ffbb1f3a516e6b59d04cd57f11d289
Zachary Radford <101583420+zachradf@users.noreply.github.com>
2022-04-27 16:24:46 -0400

    api and database

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -1,36 +1,32 @@
 /* eslint-disable no-unused-vars */
-import React, {useState} from 'react';
-import ReviewCard from './ReviewCard.jsx';
+import React, { useState } from "react";
+import ReviewCard from "./ReviewCard.jsx";
 
 const Sidebar = (props) => {
-    const { reviews } = props;
-    console.log(reviews)
+  const { reviews } = props;
+  console.log(reviews);
 
-    const reviewsArray = reviews.map((review) => {
-        return (
-            <ReviewCard
-                key={review._id}
-                rating={review.rating}
-                service_type={review.service_type}
-                review={review.review}
-                cost={review.cost}
-            />
-        )
-    })
+  const reviewsArray = reviews.map((review) => {
+    return (
+      <ReviewCard
+        key={review._id}
+        rating={review.rating}
+        service_type={review.service_type}
+        review={review.review}
+        cost={review.cost}
+      />
+    );
+  });
 
-    if (!reviews.length) {
-        return(
-            <div className="sidebar">
-                <p>Click on a location marker to see its reviews!</p>
-            </div>
-        )
-    }
+  if (!reviews.length) {
+    return (
+      <div className="sidebar">
+        <p>Click on a location marker to see its reviews!</p>
+      </div>
+    );
+  }
 
-    return(
-        <div className="sidebar">
-            {reviewsArray}
-        </div>
-    )
-}
+  return <div className="sidebar">{reviewsArray}</div>;
+};
 
-export default  Sidebar;
+export default Sidebar;
